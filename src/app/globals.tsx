02509921414f42b8882d77893c2abebafdc7add4
infin9 <ferriscooza918@gmail.com>
2022-11-07 18:22:43 +0530

    mainnet contract addresses

--- src/app/globals.tsx
+++ src/app/globals.tsx
@@ -7,37 +7,37 @@ export const SUPPORTED_CHAINS = [
   {
     name: 'Ethereum',
     id: 1,
-    contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
+    contractAddress: '0xc2289c8CC82b0010071FEE509233C3F8B45E1484',
   },
   {
     name: 'Binance Smart Chain',
     id: 56,
-    contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
+    contractAddress: '0x3Cc4B318A852C1C0a5A45aBCD260FAb136aB2784',
   },
   {
     name: 'Polygon',
     id: 137,
-    contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
+    contractAddress: '0x2C4b68C1D67f50dA4f87c13a54fE3Afd3ba4Fe90',
   },
   {
     name: 'Avalanche C-Chain',
     id: 43114,
-    contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
+    contractAddress: '0x579AbfC42980c56f87BCffCDe07a93c00a8733a1',
   },
-  {
-    name: 'Optimism',
-    id: 10,
-    contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
-  },
+  // {
+  //   name: 'Optimism',
+  //   id: 10,
+  //   contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
+  // },
   {
     name: 'Moonriver',
     id: 1285,
-    contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
+    contractAddress: '0x4010D4d88cca02572b38AD9542Dd6C7524914810',
   },
   {
     name: 'Arbitrum One',
     id: 42161,
-    contractAddress: '0x554ed5BA071205eDC6d9a5BdF86a1E93cBC3AD2c',
+    contractAddress: '0xEeFf4Ae9A80c97b779499aD73bfDE209aA48db1F',
   },
 ];
 
